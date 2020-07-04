8aa3179a41c5b3186795f6afa20f1b8dfbe382a4
iliyand <iliqn1994@gmail.com>
2020-07-04 12:51:39 +0100

    Initial serverless setup

---
+++ recipes.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const serverless = require("serverless-http");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// const connection = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: "recipes"
+// });
+
+module.exports.handler = async (event, context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Go Serverless v1.0! Your function executed successfully!",
+      input: event
+    })
+  };
+};
